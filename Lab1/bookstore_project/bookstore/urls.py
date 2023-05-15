e668f644057dc6743e8e08a4f2b03e3b703e9002
Omar Walid <omo_1712@yahoo.com>
2023-05-15 17:42:01 +0300

    add lab1

--- Lab1/bookstore_project/bookstore/urls.py
+++ Lab1/bookstore_project/bookstore/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.create_book, name='book_create'),
     path('<int:pk>/', views.show_book, name='book_detail'),
     path('<int:pk>/edit/', views.edit_book, name='book_edit'),
+    path('<int:pk>/delete/', views.delete_book, name='book_delete'),
 ]
--- Lab1/bookstore_project/bookstore/views.py
+++ Lab1/bookstore_project/bookstore/views.py
@@ -2,61 +2,72 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 
+
 books = []
+book_id_counter = 1
 
 
 def create_book(request):
+    global book_id_counter
     if request.method == "POST":
         title = request.POST["title"]
         author = request.POST["author"]
         published_year = request.POST["published_year"]
-        book = {"title": title, "author": author,
-                "published_year": published_year}
+        book = {"id": book_id_counter, "title": title,
+                "author": author, "published_year": published_year}
+        books.append(book)
+        book_id_counter += 1
         messages.success(request, "Book added successfully.")
         return redirect("home")
-        # return HttpResponse("Book added successfully.")
     else:
         return render(request, "create_book.html")
 
 
-def show_book(request):
+def show_book(request, pk):
+    book = None
+    for b in books:
+        if b['id'] == pk:
+            book = b
+            break
+    # book = next((b for b in books if b["id"] == int(pk)), None)
+    if book:
+        return render(request, "show_book.html", {"book": book})
+    else:
+        return HttpResponse("Book not found.")
+
+
+def edit_book(request, pk):
+    book = next((b for b in books if b["id"] == int(pk)), None)
     if request.method == "POST":
-        title = request.POST["title"]
-        book = next((b for b in books if b["title"] == title), None)
         if book:
-            return render(request, "show_book.html", {"book": book})
+            book["title"] = request.POST["title"]
+            book["author"] = request.POST["author"]
+            book["published_year"] = request.POST["published_year"]
+            messages.success(request, "Book updated successfully.")
+            return redirect("list_books")
         else:
             return HttpResponse("Book not found.")
     else:
-        return render(request, "show_book_form.html")
-
-
-def edit_book(request):
-    if request.method == "POST":
-        title = request.POST["title"]
-        book = next((b for b in books if b["title"] == title), None)
         if book:
-            book["title"] = request.POST["new_title"]
-            book["author"] = request.POST["new_author"]
-            book["published_year"] = request.POST["new_published_year"]
-            return HttpResponse("Book updated successfully.")
+            return render(request, "edit_book_form.html", {"book": book})
         else:
             return HttpResponse("Book not found.")
-    else:
-        return render(request, "edit_book_form.html")
 
 
-def delete_book(request):
+def delete_book(request, pk):
+    book = next((b for b in books if b["id"] == int(pk)), None)
     if request.method == "POST":
-        title = request.POST["title"]
-        book = next((b for b in books if b["title"] == title), None)
         if book:
             books.remove(book)
-            return HttpResponse("Book deleted successfully.")
+            messages.success(request, "Book deleted successfully.")
+            return redirect("list_books")
         else:
             return HttpResponse("Book not found.")
     else:
-        return render(request, "delete_book_form.html")
+        if book:
+            return render(request, "delete_book.html", {"book": book})
+        else:
+            return HttpResponse("Book not found.")
 
 
 def list_books(request):
